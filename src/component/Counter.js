ba92bb8f03c012974bf4017ae13cfb6ea9881425
SadafKhan434 <sadafkhan2192@gmail.com>
2026-08-10 19:28:43 +0530

    Increase And Decrease Updated

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -7,8 +7,14 @@ const Counter=()=>{
  const incrementHandler=()=>{
    dispatch({type:'increment'})
  }
+ const increaseHandler=()=>{
+    dispatch({type:'increase',amount:5})
+ }
  const decrementHandler=()=>{
     dispatch({type:'decrement'})
+ }
+ const decreaseHandler=()=>{
+    dispatch({type:'decrease',amount:5})
  }
 const toggleCounterHandler=()=>{}
 
@@ -19,7 +25,9 @@ return(
         <div className={classes.value}> {counter}</div>
         <div>
             <button onClick={incrementHandler}>Increment</button>
+            <button onClick={increaseHandler}>Increase By 5</button>
             <button onClick={decrementHandler}>Decrement</button>
+            <button onClick={decreaseHandler}>Decrease By 5</button>
         </div>
         <button onClick={toggleCounterHandler}> TOGGLE COUNTER </button>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,19 @@ if(action.type==='increment'){
     counter:state.counter+1
 }
 }
+if(action.type==='increase'){
+    return{
+        counter:state.counter + action.amount,
+    }
+}
 if(action.type==='decrement'){
     return{
         counter:state.counter-1
+    }
+}
+if(action.type==='decrease'){
+    return{
+        counter:state.counter - action.amount,
     }
 }
 return state;
